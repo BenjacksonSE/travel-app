70ceadadb8cdd3053421db039e303cb9cc7f3b8d
Ben Jackson <benjacksonSE@gmail.com>
2022-01-25 16:48:50 -0500

    adding new main page

--- client/src/UI/Logo/Logo.js
+++ client/src/UI/Logo/Logo.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import classes from "./Logo,modules.scss"
+import LeafSVG from "../../assets/LeafSVG"
 
 const Logo = () => {
-  return <a href="/" className={classes.logo}></a>;
+  return <a href="/" className={classes.logo}>
+    <LeafSVG fillColor={"white"}/>
+    <span>
+       FlataItinerary
+    </span>
+  </a>;
 };
 
 export default Logo;
